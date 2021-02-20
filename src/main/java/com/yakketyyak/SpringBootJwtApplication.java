4a6a04bc881d947c6de86337f28a2f726492f1dc
Gnongo Patrick BEUGRE <patrickbeugre@pabeu-pc.local>
2021-02-20 11:32:05 +0100

    Add vault to store database secret

--- src/main/java/com/yakketyyak/SpringBootJwtApplication.java
+++ src/main/java/com/yakketyyak/SpringBootJwtApplication.java
@@ -1,5 +1,8 @@
 package com.yakketyyak;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,11 +15,20 @@ public class SpringBootJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwtApplication.class, args);
+
 	}
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Value("${secret}")
+	String password;
+
+	@PostConstruct
+	private void postConstruct() {
+		System.out.println("My password is: " + password);
 	}
 
 }
